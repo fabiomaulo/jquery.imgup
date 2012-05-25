542fed5fa574cc9fd912a711e71c2982e1429074
Fabio Maulo <fabiomaulo@gmail.com>
2012-05-25 17:47:34 -0300

    Initial upload (plugin template)

---
+++ jquery.imgup.js
@@ -0,0 +1,22 @@
+/*
+
+jQuery multi images uploader 1.0.0
+	
+Licensed under the MIT license:
+http://www.opensource.org/licenses/mit-license.php
+
+fabiomaulo@gmail.com
+
+*/
+(function ($) {
+    $.fn.imgup = function (options) {
+        var defaults = {
+        };
+
+        var options = $.extend(defaults, options);
+
+        return this.each(function () {
+            var declaredInput = $(this);
+        });
+    };
+})(jQuery);
